593abb6a9b657b042c1a153928e4c6d14127e11c
meleciosilva <mpsilva94@gmail.com>
2021-05-09 13:33:59 -0400

    add listReviews function

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -14,19 +14,41 @@ async function movieExists(req, res, next) {
   });
 }
 
+// Route Handlers
+
 async function list(req, res, next) {
-  const data = await moviesService.list();
-  const { is_showing } = req.query;
-  const byResult = is_showing === "true" ? movie => movie.is_showing === true : () => true;
-  res.json({ data: data.filter(byResult) });
+  try {
+    const data = await moviesService.list();
+    const { is_showing } = req.query;
+    const byResult = is_showing === "true" ? movie => movie.is_showing === true : () => true;
+    res.json({ data: data.filter(byResult) });
+  } catch(error) {
+      next(error)
+  }
 }
 
 function read(req, res, next) {
-  res.json({ data: res.locals.movie });
+  const { movie_id: id, title, runtime_in_minutes, rating, description, image_url } = res.locals.movie;
+  res.json({ data: { id, title, runtime_in_minutes, rating, description, image_url } });
 }
+
+async function listReviews(req, res, next) {
+  try {
+    const reviews = await moviesService.listReviews(req.params.movieId);
+    const mappedReviews = reviews.map((review, index) => {
+      const { review_id, content, score, critic_id, movie_id, surname, preferred_name, organization_name } = review;
+      return { review_id, content, score, critic_id, movie_id, critic: { surname, critic_id, preferred_name, organization_name } };
+    })
+    res.json({ data: mappedReviews })
+  } catch(error) {
+    next(error)
+  }
+}
+
 
 module.exports = {
   list,
   read: [movieExists, read],
+  listReviews: [movieExists, listReviews],
   movieExists
 }
