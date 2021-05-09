db6d65812ad8fb08865635f3da1b27e30c256ea0
meleciosilva <mpsilva94@gmail.com>
2021-05-09 17:39:05 -0400

    update drop tables seed for sqlite tests

--- src/db/seeds/00_drop_tables.js
+++ src/db/seeds/00_drop_tables.js
@@ -1,9 +1,8 @@
 module.exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex
-    .raw("TRUNCATE TABLE reviews RESTART IDENTITY CASCADE")
+  return knex("reviews").del()
     .then(() => knex("movies_theaters").del())
-    .then(() => knex.raw("TRUNCATE TABLE critics RESTART IDENTITY CASCADE"))
-    .then(() => knex.raw("TRUNCATE TABLE movies RESTART IDENTITY CASCADE"))
-    .then(() => knex.raw("TRUNCATE TABLE theaters RESTART IDENTITY CASCADE"))
+    .then(() => knex("critics").del())
+    .then(() => knex("movies").del())
+    .then(() => knex("theaters").del())
 };
