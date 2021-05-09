f1b8a4348a456f7b18aad19485216f5257f45355
meleciosilva <mpsilva94@gmail.com>
2021-05-09 13:36:17 -0400

    add destroy and update handlers

---
+++ src/reviews/reviews.controller.js
@@ -0,0 +1,45 @@
+const reviewsService = require("./reviews.service");
+
+// Validation Middleware
+
+async function reviewExists(req, res, next) {
+  const review = await reviewsService.read(req.params.reviewId);
+  const { review_id, content, score, critic_id, movie_id, surname, preferred_name, organization_name } = review;
+  const detailedReview = { review_id, content, score, critic_id, movie_id, critic: { surname, critic_id, preferred_name, organization_name } };
+  if (review) {
+    res.locals.review = detailedReview;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Review id ${req.params.reviewId} not found`
+  });
+}
+
+// Route Handlers
+
+async function read(req, res, next) {
+  res.json({ data: res.locals.review })
+}
+
+function destroy(req, res, next) {
+  reviewsService
+    .destroy(res.locals.review.review_id)
+    .then(() => res.sendStatus(204))
+    .catch(next);
+}
+
+async function update(req, res, next) {
+  const updatedReview = {
+    ...req.body.data,
+    review_id: res.locals.review.review_id
+  }
+  const data = await reviewsService.update(updatedReview);
+  res.json({ data })
+}
+
+module.exports = {
+  read: [reviewExists, read],
+  destroy: [reviewExists, destroy],
+  update: [reviewExists, update]
+}
