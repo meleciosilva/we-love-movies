1cce93a94b602deff121846bb2b94c494918b506
meleciosilva <mpsilva94@gmail.com>
2021-05-09 13:33:38 -0400

    add reviews route

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
-// const reviewsRouter = require("./reviews/reviews.router");
+const reviewsRouter = require("./reviews/reviews.router");
 
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
 app.use("/theaters", theatersRouter);
-// app.use("/reviews", reviewsRouter);
+app.use("/reviews", reviewsRouter);
 
 // Not Found Handler
 app.use((req, res, next) => {
