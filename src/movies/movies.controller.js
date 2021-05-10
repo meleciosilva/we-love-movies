bc05f6c050c32d5845393549fe14d5cca3b694f0
meleciosilva <mpsilva94@gmail.com>
2021-05-10 18:04:43 -0400

    add comments update list function to return showing movies

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -2,6 +2,7 @@ const moviesService = require("./movies.service");
 
 // Validation Middleware
 
+// checks if movie exists by movie_id
 async function movieExists(req, res, next) {
   const movie = await moviesService.read(req.params.movieId);
   if (movie) {
@@ -14,14 +15,14 @@ async function movieExists(req, res, next) {
   });
 }
 
-// Route Handlers
+// Router-level Middleware
 
+// lists all movies or movies that are currently showing
 async function list(req, res, next) {
   try {
     const data = await moviesService.list();
     const { is_showing } = req.query;
-    console.log(is_showing)
-    const byResult = is_showing === "true" ? movie => movie.is_showing === true : () => true;
+    const byResult = is_showing ? movie => movie.is_showing == true : () => true; // if given query parameter is_showing, return only movies with is_showing == true
     res.json({ data: data.filter(byResult) });
   } catch(error) {
       next(error)
@@ -33,6 +34,7 @@ function read(req, res, next) {
   res.json({ data: { movie_id, title, runtime_in_minutes, rating, description, image_url } });
 }
 
+// lists all reviews
 async function listReviews(req, res, next) {
   try {
     const reviews = await moviesService.listReviews(req.params.movieId);
@@ -42,7 +44,7 @@ async function listReviews(req, res, next) {
     })
     res.json({ data: mappedReviews })
   } catch(error) {
-    next(error)
+      next(error)
   }
 }
 
