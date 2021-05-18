a03eb5cf0a37f9054b4036acfd754fa28719869d
meleciosilva <mpsilva94@gmail.com>
2021-05-18 09:04:49 -0400

    truncate tables and restarts ID

--- src/db/seeds/00_drop_tables.js
+++ src/db/seeds/00_drop_tables.js
@@ -1,9 +1,8 @@
 module.exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex("reviews")
-    .del()
-    .then(() => knex("movies_theaters").del())
-    .then(() => knex("critics").del())
-    .then(() => knex("movies").del())
-    .then(() => knex("theaters").del());
+  // Deletes ALL existing entries and restarts ID
+  return knex.raw("TRUNCATE reviews RESTART IDENTITY CASCADE")
+    .then(() => knex.raw("TRUNCATE movies_theaters RESTART IDENTITY CASCADE"))
+    .then(() => knex.raw("TRUNCATE critics RESTART IDENTITY CASCADE"))
+    .then(() => knex.raw("TRUNCATE movies RESTART IDENTITY CASCADE"))
+    .then(() => knex.raw("TRUNCATE theaters RESTART IDENTITY CASCADE"));
 };
