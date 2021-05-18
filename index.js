ffa1cd8448dd6b88d2b0d96c32d30511e4284afd
meleciosilva <mpsilva94@gmail.com>
2021-05-18 10:41:27 -0400

    rename app to index move to root directory for vercel

---
+++ index.js
@@ -0,0 +1,35 @@
+if (process.env.USER) require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+// const morgan = require("morgan");
+
+const moviesRouter = require("./src/movies/movies.router");
+const theatersRouter = require("./src/theaters/theaters.router");
+const reviewsRouter = require("./src/reviews/reviews.router");
+
+// Application-level Middleware
+// app.use(morgan("dev")); // enables request logger
+app.use(cors()); // enables cors for the entire server/API
+app.use(express.json()); // parses incoming requests with JSON
+
+app.use("/movies", moviesRouter);
+app.use("/theaters", theatersRouter);
+app.use("/reviews", reviewsRouter);
+
+// Not Found Handler
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: `Not found: ${req.originalUrl}`,
+  });
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+  // console.error(err);
+  const { status = 500, message = "Something went wrong!" } = err;
+  res.status(status).json({ error: message });
+});
+
+module.exports = app;
